4a299a22cf369f4ec7a376612a957f5fb04ee2c4
BaeJunho <bjh4498@knu.ac.kr>
2024-11-20 10:42:49 +0900

    feat : 로그인 유저 검사를 위한 어노테이션과 리졸버 구현
    @AuthorizedUser UserInfo userInfo 를 Controller 매개변수에 삽입하여 로그인 검사 가능

---
+++ src/main/java/com/sparksInTheStep/webBoard/global/filter/MemberArgumentResolver.java
@@ -0,0 +1,46 @@
+package com.sparksInTheStep.webBoard.global.filter;
+
+import com.sparksInTheStep.webBoard.auth.application.UserService;
+import com.sparksInTheStep.webBoard.auth.application.dto.UserInfo;
+import com.sparksInTheStep.webBoard.auth.util.JwtUtil;
+import com.sparksInTheStep.webBoard.global.annotation.AuthorizedUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final UserService userService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthorizedUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        String token = webRequest.getHeader("Authorization");
+        if (token == null || !token.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("인증 방식이 잘못되었거나, 토큰이 없습니다!");
+        }
+
+        token = token.substring(7); // "Bearer " 부분을 제거
+
+        String nickname = JwtUtil.getNicknameFromToken(token);
+        if(!userService.isExistUser(nickname)) {
+            throw new IllegalArgumentException("유효하지 않은 로그인 정보입니다!");
+        }
+
+        return UserInfo.of(nickname);
+    }
+}

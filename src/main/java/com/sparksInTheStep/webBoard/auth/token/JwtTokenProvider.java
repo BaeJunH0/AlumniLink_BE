902bbbe2f7295145772b6a4003278e5e5d72023a
BaeJunho <bjh4498@knu.ac.kr>
2024-11-21 20:52:11 +0900

    refactor : 클래스 이름 변경 및 static을 non-static으로 변경
    더이상 static하지 않고, 객체를 생산하는 클래스로 변하여 Util 클래스로 보기 어려워짐, 따라서 작명도 변경

--- src/main/java/com/sparksInTheStep/webBoard/auth/token/JwtTokenProvider.java
+++ src/main/java/com/sparksInTheStep/webBoard/auth/token/JwtTokenProvider.java
@@ -1,40 +1,39 @@
-package com.sparksInTheStep.webBoard.auth.util;
+package com.sparksInTheStep.webBoard.auth.token;
 
-import com.sparksInTheStep.webBoard.auth.presentation.dto.UserRequest;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-public class JwtUtil {
-    @Value("${KEY}")
-    private static String secretKey;
+@Component
+public class JwtTokenProvider {
+    @Value("${key}")
+    private String secretKey;
 
-    @Value("${TOKEN_EXPIRE_LENGTH}")
-    private static long validTime;
+    @Value("${token-expire-length}")
+    private long validTime;
 
     /*
      * 토큰 생성 : Claim => nickname
      */
-    public static String makeAccessToken(String nickname) {
-        Long nowMillis = System.currentTimeMillis();
+    public String makeAccessToken(String nickname) {
+        long nowMillis = System.currentTimeMillis();
         Date now = new Date(System.currentTimeMillis());
 
-        String accessToken = Jwts.builder()
+        return Jwts.builder()
                 .claim("nickname", nickname)
                 .issuedAt(now)
                 .expiration(new Date(nowMillis + validTime))
                 .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
                 .compact();
-
-        return accessToken;
     }
     /*
      * 토큰에서 클레임 ( nickname ) 추출
      */
-    public static String getNicknameFromToken(String token) {
+    public String getNicknameFromToken(String token) {
         try {
             Claims claims = Jwts.parser()
                     .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))

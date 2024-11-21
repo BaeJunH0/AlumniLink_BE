cd1bc1168006a29e22b92a7d56ea63e92b95bbbd
BaeJunho <bjh4498@knu.ac.kr>
2024-11-21 20:53:40 +0900

    refactor : static 사용을 객체 생성 후 사용으로 변경

--- src/main/java/com/sparksInTheStep/webBoard/auth/presentation/UserController.java
+++ src/main/java/com/sparksInTheStep/webBoard/auth/presentation/UserController.java
@@ -3,8 +3,8 @@ package com.sparksInTheStep.webBoard.auth.presentation;
 import com.sparksInTheStep.webBoard.auth.application.UserService;
 import com.sparksInTheStep.webBoard.auth.application.dto.UserCommand;
 import com.sparksInTheStep.webBoard.auth.presentation.dto.UserRequest;
-import com.sparksInTheStep.webBoard.auth.util.JwtUtil;
-import com.sparksInTheStep.webBoard.auth.util.Token;
+import com.sparksInTheStep.webBoard.auth.token.JwtTokenProvider;
+import com.sparksInTheStep.webBoard.auth.token.Token;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class UserController {
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequest userRequest){
-        if(userService.passCheck(UserCommand.from(userRequest))){
+        if(!userService.passCheck(UserCommand.from(userRequest))){
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        String accessToken = JwtUtil.makeAccessToken(userRequest.nickname());
+        String accessToken = jwtTokenProvider.makeAccessToken(userRequest.nickname());
         return new ResponseEntity<>(Token.of(accessToken), HttpStatus.OK);
     }
 
@@ -33,7 +34,7 @@ public class UserController {
     public ResponseEntity<?> register(@RequestBody UserRequest userRequest){
         userService.makeNewUser(UserCommand.from(userRequest));
 
-        String accessToken = JwtUtil.makeAccessToken(userRequest.nickname());
+        String accessToken = jwtTokenProvider.makeAccessToken(userRequest.nickname());
         return new ResponseEntity<>(Token.of(accessToken), HttpStatus.CREATED);
     }
 }

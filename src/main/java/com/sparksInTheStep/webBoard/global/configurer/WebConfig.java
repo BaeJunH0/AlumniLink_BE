35c08b370e58f4fc27dbb73f5c29f8b239f67f8f
BaeJunho <bjh4498@knu.ac.kr>
2024-11-23 18:45:47 +0900

    fix : 로그인 관련 문제 해결
    config 파일이 없으면 argumentResolver를 쓸 수 없다는 것을... 알았습니다!

---
+++ src/main/java/com/sparksInTheStep/webBoard/global/configurer/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sparksInTheStep.webBoard.global.configurer;
+
+import com.sparksInTheStep.webBoard.global.filter.MemberArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final MemberArgumentResolver memberArgumentResolver;
+
+    public WebConfig(MemberArgumentResolver memberArgumentResolver) {
+        this.memberArgumentResolver = memberArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
+    }
+}

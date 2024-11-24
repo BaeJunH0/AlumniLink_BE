5a47566fb03a9dc7bfcf7555d2f7443d5e6de9c3
BaeJunho <bjh4498@knu.ac.kr>
2024-11-24 18:51:34 +0900

    feat : admin 비즈니스 로직 추가

--- src/main/java/com/sparksInTheStep/webBoard/auth/application/MemberService.java
+++ src/main/java/com/sparksInTheStep/webBoard/auth/application/MemberService.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -35,13 +37,57 @@ public class MemberService {
         return false;
     }
 
+    // 관리자용
+    @Transactional(readOnly = true)
+    public List<MemberInfo> readAllMembers(String nickname){
+        if(!isAdminMember(nickname)){
+            throw CustomException.of(MemberErrorCode.NO_AUTHENTICATION);
+        }
+
+        return memberRepository.findAll().stream()
+                .map(MemberInfo::from)
+                .toList();
+    }
+
+    // 관리자용
+    @Transactional
+    public void grantingMember(String adminNickname, Long memberId) {
+        if(!isAdminMember(adminNickname)){
+            throw CustomException.of(MemberErrorCode.NO_AUTHENTICATION);
+        }
+
+        Member savedMember = memberRepository.findById(memberId).orElseThrow(
+                () -> CustomException.of(MemberErrorCode.NOT_FOUND)
+        );
+        savedMember.granting();
+    }
+
+    // 관리자용
+    @Transactional
+    public void deleteMember(String adminNickname, Long memberId) {
+        if(!isAdminMember(adminNickname)){
+            throw CustomException.of(MemberErrorCode.NO_AUTHENTICATION);
+        }
+
+        memberRepository.deleteById(memberId);
+    }
+
+    @Transactional(readOnly = true)
+    public MemberInfo loginMember(String nickname){
+        return MemberInfo.from(memberRepository.findByNickname(nickname));
+    }
+
     @Transactional(readOnly = true)
     public boolean isExistMember(String nickname){
         return memberRepository.existsByNickname(nickname);
     }
 
     @Transactional(readOnly = true)
-    public MemberInfo loginMember(String nickname){
-        return MemberInfo.from(memberRepository.findByNickname(nickname));
+    public boolean isAdminMember(String nickname){
+        if(!isExistMember(nickname)){
+            throw CustomException.of(MemberErrorCode.NOT_FOUND);
+        }
+
+        return memberRepository.findByNickname(nickname).adminCheck();
     }
 }

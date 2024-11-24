c1b1812df0350bb6f283cac5fa606b1f8aa435b7
BaeJunho <bjh4498@knu.ac.kr>
2024-11-24 15:18:07 +0900

    refactor : Post -> PostInfo -> PostResponse로 흐르는 데이터 흐름을 좀 더 직관적으로 표현
    기존에 Application -> Presentation 데이터 이동에 Response를 사용하는 부분 수정

---
+++ src/main/java/com/sparksInTheStep/webBoard/post/service/dto/PostInfo.java
@@ -0,0 +1,28 @@
+package com.sparksInTheStep.webBoard.post.service.dto;
+
+import com.sparksInTheStep.webBoard.auth.application.dto.MemberInfo;
+import com.sparksInTheStep.webBoard.post.domain.Post;
+import com.sparksInTheStep.webBoard.post.domain.PostType;
+
+import java.time.LocalDateTime;
+
+public record PostInfo(
+        String title,
+        String body,
+        PostType tag,
+        MemberInfo memberInfo,
+        LocalDateTime startTime,
+        LocalDateTime modifiedTime
+        ){
+    public static PostInfo from(Post post)
+    {
+       return new PostInfo(
+               post.getTitle(),
+               post.getBody(),
+               post.getTag(),
+               MemberInfo.from(post.getMember()),
+               post.getCreatedDate(),
+               post.getLastModifiedDate()
+       );
+    }
+}

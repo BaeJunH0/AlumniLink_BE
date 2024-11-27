dcb0ffd75deb928f7d436c3fca71c8a35b4f6761
bohyeong <jbh010204@knu.ac.kr>
2024-11-27 18:38:04 +0900

    refactor : 입력 유효성 검증 및 반환 객체 수정

--- src/main/java/com/sparksInTheStep/webBoard/post/controller/PostController.java
+++ src/main/java/com/sparksInTheStep/webBoard/post/controller/PostController.java
@@ -6,6 +6,8 @@ import com.sparksInTheStep.webBoard.post.controller.dto.PostResponse;
 import com.sparksInTheStep.webBoard.post.service.PostService;
 import com.sparksInTheStep.webBoard.post.service.dto.PostCommand;
 import com.sparksInTheStep.webBoard.global.annotation.AuthorizedUser;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +18,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
-public class PostController {
+public class PostController{
     private final PostService postService;
 
     @PostMapping
     public ResponseEntity<String> savePost(
             @AuthorizedUser MemberInfo.Default memberInfo,
-            @RequestBody PostRequest request
+            @Valid @RequestBody PostRequest request
     ) {
         postService.createPost(memberInfo.nickname(), PostCommand.from(request));
         return ResponseEntity.ok("성공적으로 게시글이 등록되었습니다.");
@@ -32,17 +34,18 @@ public class PostController {
     public ResponseEntity<List<PostResponse>> getPostsByMember(
             @AuthorizedUser MemberInfo.Default memberInfo
     ) {
-        return ResponseEntity.ok(
-                postService.getPostsByMember(memberInfo).stream().
-                        map(PostResponse::from).
-                        toList()
-        );
+        List<PostResponse> response = postService
+                .getPostsByMember(memberInfo)
+                .stream()
+                .map(PostResponse::from)
+                .toList();
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping
     public ResponseEntity<String> deletePost(
             @AuthorizedUser MemberInfo.Default memberInfo,
-            @RequestBody Long postId
+            @NotNull @RequestBody Long postId
     ) throws AuthenticationException {
         postService.deletePost(memberInfo, postId);
 
@@ -50,13 +53,15 @@ public class PostController {
     }
 
     // 포스트 전체 보기 ( 글 읽기는 로그인 필요 x )
+
+
     @GetMapping
     public ResponseEntity<List<PostResponse>> getAllPosts(){
-        return ResponseEntity.ok(
-                postService.getAllPosts().
-                        stream().
-                        map(PostResponse::from).
-                        toList()
-        );
+        List<PostResponse> response = postService
+                .getAllPosts()
+                .stream()
+                .map(PostResponse::from)
+                .toList();
+        return ResponseEntity.ok(response);
     }
 }

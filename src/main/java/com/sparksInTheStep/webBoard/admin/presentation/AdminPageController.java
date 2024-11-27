3181056e90a379540278bb3c92f471386874d872
BaeJunho <bjh4498@knu.ac.kr>
2024-11-27 10:26:06 +0900

    chore : 사용하지 않는 import문 삭제

--- src/main/java/com/sparksInTheStep/webBoard/admin/presentation/AdminPageController.java
+++ src/main/java/com/sparksInTheStep/webBoard/admin/presentation/AdminPageController.java
@@ -1,11 +1,8 @@
 package com.sparksInTheStep.webBoard.admin.presentation;
 
-import com.sparksInTheStep.webBoard.auth.application.dto.MemberInfo;
-import com.sparksInTheStep.webBoard.global.annotation.AuthorizedUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/admin")

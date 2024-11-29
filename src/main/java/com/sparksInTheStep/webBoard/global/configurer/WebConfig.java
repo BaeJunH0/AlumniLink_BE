8716485122ae9637449ad8c2647c9e5a9e861fa1
BaeJunho <bjh4498@knu.ac.kr>
2024-11-29 13:55:54 +0900

    feat : CORS error 설정

--- src/main/java/com/sparksInTheStep/webBoard/global/configurer/WebConfig.java
+++ src/main/java/com/sparksInTheStep/webBoard/global/configurer/WebConfig.java
@@ -3,6 +3,7 @@ package com.sparksInTheStep.webBoard.global.configurer;
 import com.sparksInTheStep.webBoard.global.filter.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true);
     }
 }

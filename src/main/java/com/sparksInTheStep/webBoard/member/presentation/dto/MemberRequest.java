e1cf324eaa877d7adef36b2acd396b03b30b0df8
BaeJunho <bjh4498@knu.ac.kr>
2024-12-30 15:20:32 +0900

    refactor : 사용하지 않는 유효성 검증 제외

--- src/main/java/com/sparksInTheStep/webBoard/member/presentation/dto/MemberRequest.java
+++ src/main/java/com/sparksInTheStep/webBoard/member/presentation/dto/MemberRequest.java
@@ -3,9 +3,8 @@ package com.sparksInTheStep.webBoard.member.presentation.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record MemberRequest(
-        @NotBlank
         String nickname,
-        @NotBlank
+
         String password
 ) {
 

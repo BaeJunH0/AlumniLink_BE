44da9d6136756ed5006b9ec29acd21819163e7c4
BaeJunho <bjh4498@knu.ac.kr>
2024-12-30 19:38:34 +0900

    refactor : 필요없는 ExceptionHandler 삭제

--- src/main/java/com/sparksInTheStep/webBoard/global/errorHandling/CustomExceptionHandler.java
+++ src/main/java/com/sparksInTheStep/webBoard/global/errorHandling/CustomExceptionHandler.java
@@ -5,14 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -31,22 +27,6 @@ public class CustomExceptionHandler {
 
         return new ResponseEntity<>(problemDetail, exception.getHttpStatus());
     }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ProblemDetail> methodArgumentNotValidException(
-            MethodArgumentNotValidException e
-    ) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        Map<String, Object> errors = new HashMap<>();
-        e.getAllErrors()
-                .forEach(
-                        field -> errors.put(((FieldError) field).getField(), field.getDefaultMessage()));
-
-        problemDetail.setTitle("V000");
-        problemDetail.setProperties(errors);
-        return ResponseEntity.badRequest().body(problemDetail);
-    }
-
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ProblemDetail> handleException(

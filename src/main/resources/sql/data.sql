f5c8e6ebc8e93af14f163bdcc6004f56d0ca301c
BaeJunho <bjh4498@knu.ac.kr>
2025-01-02 19:03:58 +0900

    refactor : 도메인 필드 변경에 따른 수정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO member (nickname, password, admin, employed)
-VALUES ('admin', '00000000-02ca-001f-ffff-fffffd35ffe0', true, false);
+INSERT INTO member (email, nickname, password, admin, employed, git_link, resume_link)
+VALUES ('admin@gmail.com', 'admin', '00000000-02ca-001f-ffff-fffffd35ffe0', true, false, 'a', 'a');

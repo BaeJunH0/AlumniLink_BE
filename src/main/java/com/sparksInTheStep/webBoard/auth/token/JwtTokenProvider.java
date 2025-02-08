136f108e54d40d216044aea4cc01f68292158482
BaeJunho <bjh4498@knu.ac.kr>
2025-02-08 18:31:26 +0900

    refactor : access 토큰 유효 시간 검사 추가

--- src/main/java/com/sparksInTheStep/webBoard/auth/token/JwtTokenProvider.java
+++ src/main/java/com/sparksInTheStep/webBoard/auth/token/JwtTokenProvider.java
@@ -3,6 +3,7 @@ package com.sparksInTheStep.webBoard.auth.token;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -95,6 +96,29 @@ public class JwtTokenProvider {
             return claims.get("type", String.class);
         } catch(Exception e){
             return "null";
+        }
+    }
+
+    /*
+     * 토큰의 유효 시간 확인
+     */
+    public boolean isTokenExpired(String token) {
+        try {
+            // 토큰을 파싱하여 Claims 객체를 얻음
+            Claims claims = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            // 만료 시간 가져오기
+            Date expiration = claims.getExpiration();
+
+            // 현재 시간과 만료 시간을 비교하여 만료 여부를 판단
+            return expiration.before(new Date());
+        } catch (SignatureException e) {
+            // 유효하지 않은 서명일 경우 예외 처리
+            return true;
         }
     }
 }

b53b279db9d1c85d7ba52ad3f80d3eef2d8487ca
BaeJunho <bjh4498@knu.ac.kr>
2025-03-08 15:40:55 +0900

    refactor : 프로젝트에 가입한 멤버 엔티티를 좀 더 직관적 이름으로 변경

--- src/main/java/com/sparksInTheStep/webBoard/project/persistent/ProjectMemberRepository.java
+++ src/main/java/com/sparksInTheStep/webBoard/project/persistent/ProjectMemberRepository.java
@@ -1,14 +1,14 @@
 package com.sparksInTheStep.webBoard.project.persistent;
 
-import com.sparksInTheStep.webBoard.project.doamin.JoinedProject;
+import com.sparksInTheStep.webBoard.project.doamin.ProjectMember;
 import com.sparksInTheStep.webBoard.member.domain.Member;
 import com.sparksInTheStep.webBoard.project.doamin.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JoinedProjectRepository extends JpaRepository<JoinedProject, Long> {
-    Page<JoinedProject> findJoinedProjectsByMember(Pageable pageable, Member member);
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+    Page<ProjectMember> findJoinedProjectsByMember(Pageable pageable, Member member);
 
     Boolean existsByMemberAndProject(Member member, Project project);
 
